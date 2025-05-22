d6069bd45d03f2a756d860b9ba037885e14eca02
angcyo <angcyo@126.com>
2025-05-22 18:31:20 +0800

    + clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use clap::Parser;
+use colored::Colorize;
+
+mod args;
 mod https;
 mod macros;
 
@@ -18,14 +22,11 @@ fn test_macro() {
     dbg!(10 / 3);
 }
 
-#[tokio::main]
-#[allow(arithmetic_overflow)]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    //test_macro();
-
+async fn test_html2md() -> Result<(), Box<dyn std::error::Error>> {
     let url = "https://www.rust-lang.org";
     let output = ".output/rust.md";
 
+    ptl!("Request {}", url);
     let body = https::get_url_text(url).await?;
 
     // Create output directory
@@ -37,5 +38,24 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let md = html2md::parse_html(&body);
     std::fs::write(output, md.as_bytes()).unwrap();
     ptl!("Converted markdown has been saved in {}.", output);
+    Ok(())
+}
+
+#[tokio::main]
+#[allow(arithmetic_overflow)]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    //test_macro();
+    //test_html2md().await?;
+    let args = args::Args::parse();
+    
+    for _ in 0..args.count {
+        if let Some(name) = args.name.as_ref() {
+            ptl!("Hello {}!", name.green().bold()); //>Hello angcyo!
+            //ptl!("Hello {:?}!", name);//>Hello "angcyo"!
+        } else {
+            println!("Please use {} run.", "`--name xxx`".red().bold());
+        }
+        //println!("Hello {:?}!", args.name.as_ref().unwrap());
+    }
     Ok(())
 }
