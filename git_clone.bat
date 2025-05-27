7e1a603cb0405015c81376f4c70a74afe9593e50
angcyo <angcyo@126.com>
2025-05-27 11:22:43 +0800

    * fix base

--- git_clone.bat
+++ git_clone.bat
@@ -4,3 +4,16 @@ chcp 65001 >> nul
 
 git fetch
 git rebase origin/main
+
+set "folder=rust_crates"
+
+if exist "%CD%\%folder%\" (
+    cd %folder%
+    echo 准备拉取仓库：%folder%
+    git fetch
+    git rebase origin/main
+    cd ..
+) else (
+    echo 准备克隆仓库：%folder%
+    git clone "git@github.com:LaserPeckerIst/%folder%.git"
+)
