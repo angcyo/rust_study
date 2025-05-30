d241e26525f22f3b3d87e4cc2250598439d36626
angcyo <angcyo@126.com>
2025-05-30 09:51:41 +0800

    * fix base

--- macro_demo/src/main.rs
+++ macro_demo/src/main.rs
@@ -1,10 +1,20 @@
-use rc_macro::print_token_stream;
+use rc_macro::{print_token_stream, DerivePrintToken};
 
 ///
 /// @author <a href="mailto:angcyo@126.com">angcyo</a>
 /// @date 2025/05/29
 ///
 fn main() {
-    //println!("Hello, world!");
+    let a = MacroStruct {
+        name: "angcyo".to_string(),
+        name2: Some("angcyo2".to_string()),
+    };
+    println!("{:?}", a);
     print_token_stream!(SELECT * FROM users WHERE age > 10);
+}
+
+#[derive(Debug, DerivePrintToken)]
+pub struct MacroStruct {
+    name: String,
+    pub name2: Option<String>,
 }
