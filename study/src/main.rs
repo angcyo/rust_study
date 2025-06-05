56fcde9a3179802309c3cb71e769767cf33d4292
angcyo <angcyo@126.com>
2025-06-05 12:18:38 +0800

    + thread

--- study/src/main.rs
+++ study/src/main.rs
@@ -2,15 +2,16 @@ use colored::Colorize;
 use lazy_static::lazy_static;
 use rc_basis::ptl;
 use std::collections::HashMap;
+use std::sync::Mutex;
 
 mod android;
+mod badges;
 mod https;
 mod mail;
 mod pdf;
 mod protocol;
-mod web;
-mod badges;
 mod threads;
+mod web;
 
 #[allow(dead_code)]
 fn ensure_output_dir_exist() {
@@ -105,6 +106,7 @@ fn test_image() {
 //--
 
 lazy_static! {
+    /// 不可变延迟初始化的静态map对象
     static ref MAP: serde_json::Map<String, serde_json::Value> = {
         let mut m = serde_json::Map::new();
         m.insert(
@@ -120,6 +122,17 @@ lazy_static! {
             serde_json::Value::String("baz".to_string()),
         );
         m
+    };
+}
+
+lazy_static! {
+    /// 可变的延迟初始化的静态map对象
+    static ref MUT_MAP: Mutex<HashMap<String, String>> = {
+        let mut map = HashMap::new();
+        map.insert("0".to_string(), "0".to_string());
+        map.insert("1".to_string(), "1".to_string());
+        map.insert("2".to_string(), "true".to_string());
+        Mutex::new(map)
     };
 }
 
@@ -161,7 +174,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 #[cfg(test)]
 mod tests {
-    use crate::{ensure_output_dir_exist, ptl};
+    use crate::{ensure_output_dir_exist, ptl, MAP, MUT_MAP};
     use rc_basis::get_current_dir;
 
     /// [study/src/main.rs:183:9]->/Users/angcyo/project/rust/rust_study/study
@@ -182,5 +195,28 @@ mod tests {
         let output_image_path = format!("../.output/{output_image_name}");
         rc_image::convert::resize_image_file(image_path, width, height, output_image_path.as_str())
             .unwrap()
+    }
+
+    #[test]
+    fn test_lazy_static() {
+        rc_log::init_log();
+        MAP.keys().for_each(|k| {
+            rc_log::log::info!("key:{k} value:{}", MAP.get(k).unwrap());
+        });
+
+        //--
+
+        let mut mut_map = MUT_MAP.lock().unwrap();
+        mut_map.remove("0");
+        mut_map.remove("2");
+        mut_map.remove("3");
+        mut_map.insert("new_key".to_string(), "new_value".to_string());
+        mut_map.keys().for_each(|k| {
+            rc_log::log::info!("mut_key:{k} mut_value:{}", mut_map.get(k).unwrap());
+        });
+        mut_map.clear();
+        mut_map.keys().for_each(|k| {
+            rc_log::log::info!("mut_key:{k} mut_value:{}", mut_map.get(k).unwrap());
+        });
     }
 }
