c3710f53045f2c7207f3b724a4c604d9dd61b7c1
angcyo <angcyo@126.com>
2025-07-19 17:44:28 +0800

    + svg offset test

--- study/src/geo/mod.rs
+++ study/src/geo/mod.rs
@@ -1,7 +1,8 @@
 #[cfg(test)]
 mod tests {
     use crate::test::{get_test_file_path, save_and_open_file};
-    use geo::{ConcaveHull, ConvexHull};
+    use geo::ConcaveHull;
+    use geo_offset::Offset;
     use geo_svg_io2::geo_svg_reader::{svg_to_geometry, svg_to_geometry_collection};
     use geo_svg_io2::geo_svg_writer::{ToSvg, ToSvgString};
     use rc_basis::files::read_file_to_string;
@@ -65,5 +66,33 @@ mod tests {
         let svg_path = convex_hull.to_svg_string(); //svg path
 
         save_and_open_file(name, svg.as_bytes());
+    }
+
+    /// 测试偏移
+    #[test]
+    fn test_geo_offset() {
+        /*let name = "star.svg";
+        let output_name = "star_offset.svg";*/
+
+        /*let name = "love.svg";
+        let output_name = "love_offset.svg";*/
+
+        /*let name = "love2.svg";
+        let output_name = "love2_offset.svg";*/
+
+        let name = "xtool-export-star.svg";
+        let output_name = "xtool-export-star-offset.svg";
+
+        let svg = read_file_to_string(get_test_file_path(name).as_str()).unwrap();
+        //println!("{}", svg);
+        //open_file_with_sys(&svg);
+
+        let parsed_svg = svg_to_geometry(&svg).ok().unwrap();
+        let parsed_poly = parsed_svg.into_polygon().unwrap();
+        let convex_hull = parsed_poly.offset(10.0).ok().unwrap();
+        let svg = convex_hull.to_svg(); //svg <path> xml , 只有<path>标签的xml文档
+        let svg_path = convex_hull.to_svg_string(); //svg path
+
+        save_and_open_file(output_name, svg_path.as_bytes());
     }
 }
